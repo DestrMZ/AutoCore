c89cccff3f1e1cbb42b2ca9288441745a511f2b3
IvanMaslennikov <roma.axe.99@gmail.com>
2024-10-12 12:18:47 +0300

    !

---
+++ carRepairApp/View/AddPhotoCarView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPhotoCarView.swift
+//  carRepairApp
+//
+//  Created by Ivan Maslennikov on 12.10.2024.
+//
+
+import SwiftUI
+
+struct AddPhotoCarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPhotoCarView()
+}
